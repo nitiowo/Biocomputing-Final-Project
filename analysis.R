acd027ae4953cbcb737ea0085579652cbf5213b4
James Magas <jmagas@nd.edu>
2023-12-03 19:01:14 -0500

    Creation of the two needed code files with comments on what they need to do

---
+++ analysis.R
@@ -0,0 +1,15 @@
+#James Magas
+#2023-03-12
+#Biocomputing Final Project
+#analysis portion
+
+#use source() function to load
+#functions defined in supportingFunctions.R
+#compile all data into single .csv file
+#process data included in the entire data set
+#to answer the two questions
+#1. in what country (X or Y) did the disease outbreak
+#likely begin?
+#and
+#2. If country Y develops a vaccine for the disease, is
+#it likely to work for citizens of country X?
