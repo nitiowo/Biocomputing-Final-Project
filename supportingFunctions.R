b6199c46d2a53a7ecd7aa7b32714ca65f2b611d9
mkrause3 <mkrause3@nd.edu>
2023-12-04 19:58:20 -0500

    started work on summarizing function. includes analysis for the total screens run, the percent positive, and percent male and female.

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -28,7 +28,47 @@ country_full <- strsplit(filename, "[^a-zA-Z0-9]")[[1]][1]
 country <- strsplit(country, "[a-z]")[[1]][8]
 screen_120.csv$country = country
 
+# make a for loop to add columns to each csv file
+filelist <- list.files()
+length <- length(filelist)
+for (i in 1:length) {
+  print(i)
+  # create the name of each file as the file name according to the iteration i
+  name <- as.character(filelist[i])
+  # use read.csv on the file for iteration i and name it the according name
+  assign(name, read.csv(as.character(filelist[i])))
+  ## add day of year column
+  dayofYear <- strsplit(as.character(filelist[i]), "[^a-zA-Z0-9]")[[1]][2]
+  ######### this doesn't work
+  # use some sort of function that creates a column?
+  # name$dayofYear = dayofyear
+  ## add country column
+  country <- "X"
+}
+
 
 # compile <- function()
 
 #### summarize compiled data ----
+##%% issues: how to return multiple variables in the function
+
+summary <- function(datafile) {
+  # read in data file
+  datafile <- read.csv(datafile)
+  ## number of screens run
+  # create variable for the number of screens run
+  screensrun <- nrow(datafile)
+  ## percent positive
+  # select only the columns that have at least one marker
+  positive <- datafile[apply(datafile[, 3:12], 1, sum) > 0, ]
+  posrows <- nrow(positive)
+  percentpos <- posrows / screensrun
+  ## percent female
+  female <- datafile[datafile$gender == "female", ]
+  femalerows <- nrow(female)
+  percentfemale <- femalerows / screensrun
+  ## percent male
+  percentmale <- (screensrun - femalerows) / screensrun
+  return(percentmale)
+}
+
