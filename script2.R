fc84e9bf58985cd554b9f30ba6b8a77c942f0b9f
Judith Lanahan <jlanahan@nd.edu>
2023-12-08 12:47:58 -0500

    did this change anything

--- script2.R
+++ script2.R
@@ -1,1 +1,15 @@
-### 
+### second try
+
+
+
+fileConverter<-function(workingDir){
+  setwd(workingDir)
+  files2convert<-list.files(workingDir, pattern = ".txt")
+  for(i in 1:length(files2convert)){
+    nameElements<-strsplit(files2convert[i], split = "\\.")
+    dfHolder<-read.table(files2convert[i], header = TRUE, sep = " ")
+    write.csv(dfHolder, paste(nameElements[1],".csv"), row.names=FALSE, col.names=TRUE)
+  }
+}
+
+fileConverter("C:/Users/Judith Lanahan/OneDrive/Desktop/Biocomputing/R/FinalProject/Biocomputing-Final-Project/countryY")
