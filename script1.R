d14c6debc769fbe5249b902531874b89205560ac
Judith Lanahan <jlanahan@nd.edu>
2023-12-08 15:16:00 -0500

    some confusing stuff about combining dataframes bro

--- script1.R
+++ script1.R
@@ -36,3 +36,50 @@ nameElements
 paste("countryY/",screenListY[1],".csv", sep = "")
 
 
+setwd("C:/Users/Judith Lanahan/OneDrive/Desktop/Biocomputing/R/FinalProject/Biocomputing-Final-Project")
+
+
+
+## second function practice stuff
+# get number of directories from user
+dirNum<-readline("How many directories?  ")
+# make a list whose length is the number of user inputed directories
+dirList<-numeric(dirNum)
+# loop through number of directories and prompt user to input the name of the directory for that iteration
+# variable dirList[i] will replace the placeholder 0 with the directory path in that list
+for(i in 1:dirNum){
+  dirList[i]<-readline(prompt = paste("Path to directory ",i, ": "))
+}
+
+dirList
+
+# make a for loop to get the files from the directory
+
+for(i in 1:length(dirList)){
+  countryElem<-dirList[i]
+  files<-list.files(dirList[i], pattern=".csv")
+  dataList<-numeric(length(files))
+  for(j in 1:length(files)){
+    nameElem<-unlist(strsplit(files[j], "_"))
+    dayElem<-unlist(strsplit(nameElem[2], split = "\\."))
+    dayofYear<-dayElem[1]
+    df<-read.csv(files[j], header = TRUE)
+    for(z in 1:nrow(df)){
+      df$Country[z]<-countryElem
+      df$dayOfYear[z]<-dayElem
+    }
+    dataList[j]<-df
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
