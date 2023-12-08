bc742dbf1986af60c0ab7f0bb640cba6bf0e7b1b
Judith Lanahan <jlanahan@nd.edu>
2023-12-08 13:25:37 -0500

    now we have countryY lol

--- script2.R
+++ script2.R
@@ -6,10 +6,18 @@ fileConverter<-function(workingDir){
   setwd(workingDir)
   files2convert<-list.files(workingDir, pattern = ".txt")
   for(i in 1:length(files2convert)){
-    nameElements<-strsplit(files2convert[i], split = "\\.")
+    nameElements<-unlist(strsplit(files2convert[i], split = "\\."))
     dfHolder<-read.table(files2convert[i], header = TRUE, sep = " ")
-    write.csv(dfHolder, paste(nameElements[1],".csv"), row.names=FALSE, col.names=TRUE)
+    write.csv(dfHolder, paste(nameElements[1],".csv"), row.names=FALSE)
   }
 }
 
 fileConverter("C:/Users/Judith Lanahan/OneDrive/Desktop/Biocomputing/R/FinalProject/Biocomputing-Final-Project/countryY")
+
+
+sample_string<-"I love bio computing"
+banana<- strsplit(sample_string, " ")
+banana[1]
+banana[[1]]
+banana2<-unlist(banana)
+banana2[1]
