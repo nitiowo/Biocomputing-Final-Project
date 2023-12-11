fceb914d1986419a753127c0410f7e0d57ed313e
Vivian <yyang29@nd.edu>
2023-12-11 16:12:31 -0500

    changes made in 12/11/2023

--- Biocomputing_final_YY.R
+++ Biocomputing_final_YY.R
@@ -1,14 +1,23 @@
 library(ggplot2)
 library(cowplot)
 #set pathway
-setwd("/Users/vivianyang/Documents/GitHub/Biocomputing-Final-Project/countryX")
+directory_X <- "/Users/vivianyang/Documents/GitHub/Biocomputing-Final-Project/countryX"
+directory_Y <- "/Users/vivianyang/Documents/GitHub/Biocomputing-Final-Project/countryY"
 
-
-
-  csv_file <- list.files(pattern = "\\.csv$")
-  all_csv <- list()
-  # Loop through the files and read each one
-  for (file in csv_file) {
-    data <- read.csv(file)
-    all_csv[[file]] <- data
-  }
+# Country X's files are already in comma-delimited format;
+# Country Y need to be changed from tab-delimited to comma-delimited
+convert_Y_to_csv <- function(directory) {
+  # Get a list of all files in the directory
+  all_file <- list.files(directory)
+  # Loop through all the files
+  for(file in all_file) {
+    # Use the original file name
+    file_name <- strsplit(file,".txt")
+    # add ".csv" to the end of file_name
+    csv_file_name <- paste0(file_name, ".csv")
+    # Read the tab-delimited file
+    data <- read.table(file, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    # Write into a csv file
+    write.csv(data, csv_file_name)
+  }}
+convert_Y_to_csv(directory_Y)
