534105abe2ff1c9ea5aae24b85c1f12bde6a9a2a
Grace Waddell <gwaddel3@nd.edu>
2023-12-12 19:43:33 -0500

    Update 6

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -2,20 +2,31 @@
 
 #Suggested functions:
 #Write a function that converts all files in a directory with space- or tab-delimited 
-#data (.txt) into csv files
+#data (.txt) into csv files.
+
+#Process:
+#1. Create a function with a punctuation argument and a file argument.
+#2. Set up an if/else if conditional for the space or tab options.
+  #i. Read the file with the file's given separator.
+  #ii. Write the file with the comma as a separator.
+  #iii. Return the file.
 #See tutorial 7 exercise 1 (that problem is .csv to .txt, but it's the same idea)
 
+#User instructions for the function:
+#The function can be used as follows: commaTabtoCSV(punctuation,file), where punctuation
+  #can either be "space" or "tab", and the file argument is the file path.
+
 commaTabtoCSV <- function(punctuation,file){
-  if (punctuation == "space"){
-    readFile <- read.table(file=file, sep = " ")
-    csvFile <- write.table(readFile, sep = ",")
-    return(csvFile)
-  }else if(punctuation == "tab"){
-    readFile <- read.table(file=file, sep = "\t")
-    csvFile <- write.table(readFile, sep = ",")
-    return(csvFile)
-  }else{
-    print("Format is not supported by this function.")
+  if (punctuation == "space"){ #for files with values separated by spaces
+    readFile <- read.table(file=file, sep = " ") #read the file
+    csvFile <- write.table(readFile, sep = ",") #rewrite the file with commas as separators
+    return(csvFile) #return the file as a csv
+  }else if(punctuation == "tab"){ #for the files with values separated by tabs
+    readFile <- read.table(file=file, sep = "\t") #read the file
+    csvFile <- write.table(readFile, sep = ",") #rewrite the file with commas as separators
+    return(csvFile) #return the file as a csv
+  }else{ #in case an inappropriate punctuation argument is given
+    print("Format is not supported by this function.") #error message
   }
 }
 
@@ -29,67 +40,92 @@ commaTabtoCSV <- function(punctuation,file){
 #sub-strings of characters. For example, if x <- "I love biocomputing", then z <-
 #strsplit(x," ") would allow you to access each word in the sentence from the 
 #contents of z.
+
 #Process:
 #1. Set working directory with setwd() to be in the right folder
 #2. Make a list of the files in the directory with list.files() in the working 
 #directory
 #3. Body of the custom function designed to combine all of this data.
 #a. Arguments of the function: list of files, country names, NA argument
-#b. Set up an empty data frame for the combined data and final data
+#b. Set empty data frames for the combined data and final data
 #c. Set up a for loop to iterate through this list of files
-#d. Obtain the country and day for the 13th and 14th rows
-#e. Add the two new columns of data for each row (using strsplit() for the date)
-#f. Do the NA option picked by the user (use if/elif statements)
-#g. Return the final .csv file
+  #i. Obtain the country and day for the 13th and 14th rows
+  #ii. Add the two new columns of data for each row (using strsplit() for the date)
+#d. Perform the NA option picked by the user (use if/elif statements)
+#e. Return the final .csv file
 #Reference the final challenge of lecture 26 for this step
 
+#User instructions for the function:
+#The user can use the function as follows: compileData(countryPaths, countryList, NAChoice),
+  #where the countryPaths are a list of the file paths to the directories for each
+  #country's data, the countryList is a list of the country names (in the same order as
+  #the provided country directories), and a NAChoice, which can be "no NA" (to exclude all
+  #NA values), "NA with warning" to keep the NA values in the combined data but to provide
+  #a warning to the user, or "keep NA" to keep all NA values without warning the user.
+
 countryPaths <- list("C:/Users/grace/Desktop/Biocomputing 2023/R/Final Project/Biocomputing-Final-Project/countryX") #list of directory paths
-countryList <- list("X")
-NAChoice <- "no NA"
+countryList <- list("X") #list of the countries
+NAChoice <- "no NA" #input for the NA Choice
 compileData <- function(countryPaths = countryPaths, countryList = countryList, NAChoice = NAChoice){ #function
-  output.csv <- data.frame()
-  FinalOutput.csv <- data.frame()
-  for(i in 1:length(countryPaths)){ #iterate through each country's directory (2)
+  output.csv <- data.frame() #create an empty dataframe for the intermediate output
+  FinalOutput.csv <- data.frame() #create an empty dataframe for the final output
+  for(i in 1:length(countryPaths)){ #iterate through each country's directory
     setwd(as.character(countryPaths[i])) #set working directory for the country
     fileList <- list.files() #generate a list of files for the directory
-    for(j in 1:length(fileList)){ #iterate through each file (56)
+    for(j in 1:length(fileList)){ #iterate through each file
       currentFile <- read.csv(fileList[j], header = TRUE) #read the current file
-      country <- as.character(countryList[i]) #get the country for each file
-      countryColumn <- rep(x = country, nrow(currentFile)) #make a vector with the length of the number of rows in the file with the country name
-      fileStrSplit <- strsplit(fileList[j], split = "_") #get the day number from the file name
-      fileStrSplitChar <- fileStrSplit[[1]][2]
+      country <- as.character(countryList[i]) #get the corresponding country for each file
+      countryColumn <- rep(x = country, nrow(currentFile)) #make a vector with the length of the number of rows in the file with the obtained country name
+      fileStrSplit <- strsplit(fileList[j], split = "_") #get the day number.csv from the file name
+      fileStrSplitChar <- fileStrSplit[[1]][2] #change this from an element of a list to a character
       dayStrSplit <- strsplit(fileStrSplitChar, split = "[.]") #split the number.csv into the number and csv; use an escape character for . as split
-      day <- as.numeric(dayStrSplit[[1]][1]) #get the day from the split of the number.csv      dayColumn <- rep(x = day, nrow(currentFile)) #make a vector with the length of the number of rows in the file with the day
-      dayColumn <- rep(x = day, nrow(currentFile)) #make a vector with the length of the number of rows in the file with the day
-      currentFile$dayofYear <- dayColumn #create the row with country and day
-      currentFile$country <- countryColumn #create the row with country and day
-      if(i*j == 1){
-        write.table(currentFile, file = "output.csv", sep = ",", col.names = TRUE, row.names = FALSE) #append = TRUE is not needed
-      }else{
-        write.table(currentFile, file = "output.csv", append = TRUE, sep = ",", col.names = FALSE, row.names = FALSE) #append = TRUE is needed
+      day <- as.numeric(dayStrSplit[[1]][1]) #get the day from the split of the number.csv
+      dayColumn <- rep(x = day, nrow(currentFile)) #make a vector with the day and the length of the number of rows
+      currentFile$dayofYear <- dayColumn #create the column with the day
+      currentFile$country <- countryColumn #create the column with the country
+      if(i*j == 1){ #for the first row, the column names are needed
+        write.table(currentFile, file = "output.csv", sep = ",", col.names = TRUE, row.names = FALSE) #append = TRUE is not needed, col.names is needed
+      }else{ #for every row but the first row, col.names is not needed
+        write.table(currentFile, file = "output.csv", append = TRUE, sep = ",", col.names = FALSE, row.names = FALSE) #append = TRUE is needed, col.names is not needed
       }
     }
   }
-  outputRead <- read.csv("output.csv")
-  if(NAChoice == "no NA"){   #NA options: if/else if/else loop for the 3 cases
-    for(k in 1:nrow(outputRead)){
-      if(sum(is.na(outputRead[k,1:14])) == 0){ #Use !is.na() from lecture 22 notes; the sum(is.na()) adds 1 every time there is a NA value (adds 1 for the Boolean True and 0 for the Boolean False)
-        if(k != 1){
-          write.table(outputRead[k,1:14], file = "FinalOutput.csv", sep = ",", append = TRUE, col.names = FALSE, row.names = FALSE) #col.names = TRUE is not needed for the remaining rows
-        }else{
+  outputRead <- read.csv("output.csv") #read the intermediate .csv file
+  if(NAChoice == "no NA"){   #if NAs are to be excluded
+    for(k in 1:nrow(outputRead)){ #iterate through each row of the intermediate .csv file
+      if(sum(is.na(outputRead[k,1:14])) == 0){ #Use !is.na() from lecture 22 notes; the sum(is.na()) adds 1 every time there is a NA value in a row (adds 1 for True and 0 for False)
+        if(k != 1){ #for the first row, the column names are needed
+          write.table(outputRead[k,1:14], file = "FinalOutput.csv", sep = ",", append = TRUE, col.names = FALSE, row.names = FALSE) #col.names are not needed after the first row
+        }else{ #when k = 1
           write.table(outputRead[k,1:14], file = "FinalOutput.csv", sep = ",", append = TRUE, col.names = TRUE, row.names = FALSE) #col.names = TRUE is needed for the first row
         }
       }
     }
-  }else if(NAChoice == "NA with warning"){
-    print("NA values are included in this data.")
-    FinalOutput.csv <- output.csv
-  }else if(NAChoice == "keep NA"){
-    FinalOutput.csv <- output.csv
-  }else{
-    print("Please put an accepted argument for NAChoice: no NA, NA with warning, or keep NA")
+  }else if(NAChoice == "NA with warning"){ #if NAs are to be included but a warning is provided
+    print("NA values are included in this data.") #warning to the user
+    FinalOutput.csv <- output.csv #save the intermediate output as the final output
+  }else if(NAChoice == "keep NA"){ #if NAs are to be included without a warning
+    FinalOutput.csv <- output.csv #save the intermediate output as the final output
+  }else{ #if an unacceptable input is given to the NAChoice argument
+    print("Please put an accepted argument for NAChoice: no NA, NA with warning, or keep NA") #error message
   }
   return(FinalOutput.csv) #return statement
 }
 
-compileData(countryPaths, countryList, NAChoice)
+#Write a function to summarize the compiled data set in terms of number of screens run,
+ #percent of patients screened that were infected, the percent of patients that identify
+ #as male and female, and the age distribution of patients (as a table or plot). Note that 
+ #we provide a file with the data compiled (allData.csv), so that this task is not 
+ #dependent on completion of the other tasks.
+#Process:
+#1. The instructions say that we can use the compiled allData.csv file so that this task
+  #is not dependent upon the other tasks, so read the allData.csv file.
+#2. Determine the number of screens run
+#3. Determine the percentage of patients screened that were infected
+#4. Determine the percent of patients that identify as male and the percent of patients
+  #that identify as female.
+#5. Make a graph to show the age distribution of patients (histogram using ggplot)
+
+summarizeData <- function(combinedData = "C:/Users/grace/Desktop/Biocomputing 2023/R/Final Project/Biocomputing-Final-Project/allData.csv"){
+  return()
+}
