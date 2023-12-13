9293724a4139a17f72e327e0d8160fd86d172d87
Vivian <yyang29@nd.edu>
2023-12-13 15:20:03 -0500

    changes in Dec 13th, 2023

--- Biocomputing_final_YY.R
+++ Biocomputing_final_YY.R
@@ -37,11 +37,13 @@ compile_data <- function(directory){
   # Loop through all the data file
   for(file in all_file) {
     data <- read.csv(file, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-    split_csv = strsplit(file,".csv")
-    split_year = strsplit(split_csv, "_")
-    year <- sapply(split_year, function(x) x[2])
+    split_csv <- strsplit(file,".csv")
+    splited_csv <- paste(split_csv)
+    split_year <- strsplit(splited_csv, "_")
+    year <- split_year[[1]][2]
     # Add a new column 'dayofYear'
     data$dayofYear <- c(rep(year, nrow(data)))
+    # Add a new column 'Country'
     compiled_data <- rbind(compiled_data, data)
   }
   # Write into a csv file
@@ -52,3 +54,7 @@ compile_data(directory_X)
 setwd("/Users/vivianyang/Documents/GitHub/Biocomputing-Final-Project/countryY")
 compile_data(directory_Y)
 
+compiled_country <- data.frame()
+countryX <- read.csv("X.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+countryY <- read.csv("Y.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+compiled_data <- rbind(countryX, countryY)
