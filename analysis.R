985dea07a3166a17e316fa3f3eb575b64084a9ca
cmix14 <cmix@nd.edu>
2023-12-12 20:46:11 -0500

    wrote code for and answered question 2 in analysis.R

--- analysis.R
+++ analysis.R
@@ -44,3 +44,33 @@ ggplot(sorted_data, aes(x = dayofYear,
 ##As can be seen in the graph displayed by the code above, the incidence of cases begins and continues to rise in country X (seen in blue) from day 120 on. 
 ##Country Y (seen in red) doesn't begin to see cases until around the 140th day, meaning that their first cases were most likely acquired from country X, assuming that this is an emerging disease that was first seen globally in country X.
 ##Thus, it can be reasonably concluded that the disease outbreak began in country X.
+
+#Question 2: Would a vaccine developed in country Y be able to help those in country X?
+#read in data
+data_q_2 <- read.csv('allData.csv')
+#store just the markers for each country in a variable
+x <- data_q_2[data_q_2$country=='X',3:12]
+y <- data_q_2[data_q_2$country=='Y',3:12]
+#get a summary of these markers, store in a new variable, combine into a df
+sumx <- as.numeric(colSums(x))
+sumy <- as.numeric(colSums(y))
+markers <- c('Marker1','Marker2','Marker3','Marker4','Marker5','Marker6','Marker7','Marker8','Marker9','Marker10')
+prex <- cbind(sumx,'X',markers)
+prey <- cbind(sumy,'Y',markers)
+pre <- rbind(prex,prey)
+df <- data.frame(pre)
+colnames(df) <- c('Occurrence','Country','Markers')
+df$Occurrence <- as.numeric(df$Occurrence)
+#create a bar graph to view the df
+library(ggplot2)
+ggplot(df, aes(Country,Occurrence)) +
+  geom_bar(stat='identity') + 
+  xlab('Markers Present')+
+  ylab('Occurrence of Markers')+
+  ggtitle('Frequency of Disease Markers by Country')+
+  facet_grid(.~factor(Markers,levels=markers),scales='free')
+#Answer 2: 
+##The vaccine developed in country Y would most likely not be very effective in country X.
+##As can be seen in the graph displayed by the code above, the two countries have vastly different prevalences of markers.
+##Since the markers are the different proteins displayed by the disease, this means that the proteins of the disease itself differs between the nations.
+##Because of this difference in the targeting protein, the vaccine for one country's strain of the disease would not offer the best protection for the strain from the other country.
