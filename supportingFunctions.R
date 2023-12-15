ff4d6b9b16d5a9070890aaed5055811b4c6fef1f
Matthew Eleazar <meleazar@nd.edu>
2023-12-14 20:05:20 -0500

    completed compiler function. Added a new function that initializes empty csv. Added the files resulting in our working code.

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -46,7 +46,7 @@ initialize_csv_file <- function(x){
   colnames(empty_data) <- headers
   
   # Write the empty data frame to a CSV file
-  output_file <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/all_Data.csv"  # Specify the path and file name
+  output_file <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/init_Data.csv"  # Specify the path and file name
   write.csv(empty_data, file = output_file, row.names = FALSE)
   
 }
@@ -55,30 +55,29 @@ initialize_csv_file()
 ## make one giant csv
 # Initialize csv you want to compile countryY and countryX to then pass it into the function as a parameter
 
-
 append_to_csv <- function(input_directory, output_file, initial_csv, na_handling = "warn") {
-  # Read the initial CSV file
   compiled_data <- read.csv(initial_csv, header = TRUE)
   
-  # Extract the country from the initial CSV file's directory
-  country <- basename(dirname(initial_csv))
+  directory_parts <- strsplit(basename(input_directory), "")[[1]]  # Split the directory name into individual characters
+  
+  # Extract 'Y' using strsplit()
+  country <- directory_parts[length(directory_parts)]  # Extracts the last character
   
   files <- list.files(input_directory, pattern = "\\.csv$", full.names = TRUE)
   
   for (file in files) {
-    data <- read.csv(file, header = TRUE, skip = 1)  # Skip the first row
+    data <- read.csv(file, header = TRUE)
     
-    # Extract dayofYear from the file name using strsplit()
-    filename_parts <- strsplit(basename(file), "_")[[1]]  
-    dayofYear <- filename_parts[length(filename_parts)]  
+    # Extract the number from the filename
+    filename_number <- as.numeric(gsub("\\D", "", basename(file))) # easier to replace all occurrences with gsub
     
     # Add 'country' and 'dayofYear' columns to the data
     data$country <- country
-    data$dayofYear <- dayofYear
+    data$dayofYear <- filename_number
     
-    # Append data to the compiled_data data frame
     compiled_data <- rbind(compiled_data, data)
   }
+  
   
   if (na_handling == "remove") {
     compiled_data <- na.omit(compiled_data)
@@ -91,13 +90,19 @@ append_to_csv <- function(input_directory, output_file, initial_csv, na_handling
   write.csv(compiled_data, file = output_file, row.names = FALSE)
 }
 
-initial_csv <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/all_Data.csv"
+# Note: I will assume that the user knows what the paths to their files are so they can just change these variables when
+# using the function in another file
+
+initial_csv <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/init_Data.csv"
 output_file <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/all_Data.csv"
-input_directory <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/countryY/"
+append_file <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/all_Data.csv" # needed to append country Y after appending country X to init_data
+input_directoryX <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/countryX/"
+input_directoryY <- "C:/Users/matte/Desktop/junior_fall/Biocomputing/Biocomputing-Final-Project/countryY/"
 
 
-append_to_csv(input_directory, output_file, initial_csv)
 
+append_to_csv(input_directoryX, output_file, initial_csv) # first run with just directory X
+append_to_csv(input_directoryY, output_file, append_file) # second run with both directory X and Y
 
 
 ## summarize compiled data
