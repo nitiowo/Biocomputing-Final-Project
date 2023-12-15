78abef7e2489e26c29bddde066aa354469521c18
evelasco05 <146024088+evelasco05@users.noreply.github.com>
2023-12-14 19:26:55 -0500

    final analysis script

--- analysis.R
+++ analysis.R
@@ -14,11 +14,11 @@ setwd("c:/Users/evelasco/Documents/GitHub/Biocomputing-Final-Project")
 allData<-read.csv("compiledData.csv", header=TRUE)
 
 # make data sets for each individual country
-countryX<-allData[allData$country=="countryX",]
-countryY<-allData[allData$country=='countryY',]
+countryX<-allData[allData$Country=="countryX",]
+countryY<-allData[allData$Country=='countryY',]
 
 # find the list of unique days of screening
-uniqueDays<-unique(allData$dayofYear)
+uniqueDays<-unique(allData$dayOfYear)
 
 # make an empty data frame in which to inout the number of infected individuals per country over time
 countTable<-data.frame(matrix(nrow=length(uniqueDays), ncol=3))
@@ -39,7 +39,7 @@ for(x in 1:nrow(countTable)){
   # loop through each row of the countryX dataframe
   for(i in 1:nrow(countryX)){
     # check if the day of year i in country X matches day of year x in Count table
-    if(countryX$dayofYear[i]==countTable$dayOfYear[x]){
+    if(countryX$dayOfYear[i]==countTable$dayOfYear[x]){
       # find the sum of the row for the marker columns
       rowsum<-rowSums(countryX[i,3:12])
       # if the marker has a present, the rowsum > 0 and therefore the individual is infected
@@ -61,7 +61,7 @@ for(z in 1:nrow(countTable)){
   # loop through each row of country Y data set
   for(w in 1:nrow(countryY)){
     # check if the day of year w in country Y matches day of year z in Count table
-    if(countryY$dayofYear[w]==countTable$dayOfYear[z]){
+    if(countryY$dayOfYear[w]==countTable$dayOfYear[z]){
       # find the sum of the row for the marker columns
       rowsum<-rowSums(countryY[w,3:12])
       # if the marker has a present, the rowsum > 0 and therefore the individual is infected
@@ -83,12 +83,90 @@ infectedGraph<-ggplot()+
   geom_line(data= countTable, aes(x=dayOfYear, y=countryY, color= "CountryY"), linewidth =1.5, color='skyblue') +
   xlab("Day of Year")+
   ylab("Number of Infected Individuals")+
-  ggtitle("Total Infected Per Day by Country", subtitle="plum=country X, skyblue = country Y")+
+  ggtitle("Total Infected Per Day by Country", subtitle="plum = country X, skyblue = country Y")+
   theme_minimal()+
   guides(color='none')
 
 # display the graph
 infectedGraph
 
+# Answer 1
+# The disease outbreak likely started in country X because they started having infections earlier than country Y as shown in the graph
 
-# Country
+
+# Question 2
+# If Country Y develops a vaccine for the disease, is it likely to work for citizen of Country X?
+
+# Making data frames with just the marker columns for Country X and Country Y
+marker_columns_X <- countryX[, 3:12]
+marker_columns_Y <- countryY[, 3:12]
+
+# sum the column values to find how many of each marker is present in each country
+sum_markers_X <- colSums(marker_columns_X)
+sum_markers_X
+
+sum_markers_Y <- colSums(marker_columns_Y)
+sum_markers_Y
+
+
+# make a new data frame to combine the marker sum counts for country X
+df2<-data.frame(matrix(nrow=10, ncol=3))
+# create a vector for column names
+df2colnames<-c("marker", "country", "value")
+# add column names to data frame
+colnames(df2)<-df2colnames
+# make a vector with marker names
+markers<-colnames(marker_columns_x)
+# add marker names to the data frame
+df2$marker<-markers
+
+# add the country name to the data frame
+for(row in 1:nrow(df2)){
+  df2$country[row]<-'X'
+}
+
+# add the marker counts to the data frame
+df2$value<-sum_markers_X
+
+# make a new data frame to combine the marker sum counts for country Y
+df3<-data.frame(matrix(nrow=10, ncol=3))
+# create a vector for column names
+df3colnames<-c("marker", "country", "value")
+# add column names to data frame
+colnames(df3)<-df3colnames
+# make a vector with marker names
+markers<-colnames(marker_columns_Y)
+# add marker names to the data frame
+df3$marker<-markers
+
+# add the country name to the data frame
+for(row2 in 1:nrow(df3)){
+  df3$country[row2]<-'Y'
+}
+
+# add the marker counts to the data frame
+df3$value<-sum_markers_Y
+
+# combine the data frames
+totalDF<- rbind(df2, df3)
+
+# load packages
+library(ggplot2)
+
+# graph the marker counts by Country
+graph2<-ggplot(totalDF, aes(x=marker, y =value, fill=country)) +
+  geom_bar(stat = 'identity', position='dodge')+
+  theme_bw()+
+  ggtitle('Marker Counts by Country')+
+  xlab("Markers")+
+  ylab("Count")
+
+# Display Graph
+graph2
+
+# Answer 2
+# A vaccine developed by Country Y would not likely work for Country X because the disease markers in each country
+# because the markers are present in different quantities in each country
+
+# in Country X, the primary markers present are markers 01-05.
+# in Country Y, the primary markers present are markers 06-10.
